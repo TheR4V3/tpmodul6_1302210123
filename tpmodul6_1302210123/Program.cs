e3ad8b43381d7aace3e9aead36a75e7dd5464a7a
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-03-27 00:19:16 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210123/Program.cs
+++ tpmodul6_1302210123/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string videoTitle)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999); // 5-digit random number
+        this.title = videoTitle;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int countToAdd)
+    {
+        this.playCount += countToAdd;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Video ID: " + id );
+        Console.WriteLine($"Title: " +title );
+        Console.WriteLine($"Play Count: " + playCount );
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo myVideo = new SayaTubeVideo("Tutorial Design By Contract – [MRafaEkaP]");
+        myVideo.IncreasePlayCount(1000); 
+        myVideo.PrintVideoDetails();
+    }
+}
